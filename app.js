66798178a2fc710fe7eb5b0d71105a50a712cc96
EC2 Default User <ec2-user@ip-172-31-80-183.ec2.internal>
2019-11-30 22:02:01 +0000

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+let fileNames = fs.readdirSync("resources");
 
 const app = express();
+
+app.get('/api/images/random', (req, res) => {
+    res.sendFile(path.join(__dirname, "resources", fileNames[Math.floor(Math.random()*fileNames.length)]));
+    console.log(fileNames);
+});
+
 
 app.use(express.static("public"));
 app.use(express.json());
