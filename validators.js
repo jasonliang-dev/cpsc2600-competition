710fc33888aea422a4ff3478fd6107ae5e8acb35
EC2 Default User <ec2-user@ip-172-31-37-78.ec2.internal>
2019-11-30 22:52:53 +0000

    Validator for task 3

--- validators.js
+++ validators.js
@@ -1,1 +1,23 @@
 // TODO
+const {check} = require('express-validator');
+
+
+const sort = require('./sortingHat.js');
+
+
+
+exports.studentValidator = [
+    check("name", "Your student's name is empty")
+    .trim()
+    .not().empty()
+    .escape(),
+    check("name", "Student from wrong house")
+    .custom(value => sort(value).then(house => {
+        if (house === "Griffindor"){
+            return true;
+        }
+        Promise.reject();
+    }))
+    
+    ]
+    
